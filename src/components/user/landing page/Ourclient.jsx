a43283e271e722a9571b5c88dd0d7778de9d52b3
RamzGxz <ranzdaffa32@gmail.com>
2024-05-21 11:08:51 +0700

    changes

--- src/components/user/landing page/Ourclient.jsx
+++ src/components/user/landing page/Ourclient.jsx
@@ -31,27 +31,30 @@ const Ourclient = () => {
         <div className='flex lg:w-1/2 md:w-full w-full lg:px-0 px-6 flex-col gap-3 items-start'>
           <div className='flex items-center px-3 gap-2 rounded-full border-2'>
             <div className='w-2 h-2 rounded-full bg-[#4F5CDF]'></div>
-            <p className='text-[12px] mb-0 text-gray-500'>Our Clients</p>
+            <p className='text-[12px] mb-0 text-gray-500'>Mitra Kami</p>
           </div>
           <p className='text-gray-500'>
-            At Mudapedia, we take immense pride in our diverse and esteemed clientele. Our clients span various industries and sectors, each benefiting from our innovative solutions and dedicated service. We believe in building long-term relationships based on trust, transparency, and mutual growth. <span className='text-[#4F5CDF] font-semibold'>Here are some of the valued clients who have chosen Mudapedia as their trusted partner:</span>
+            At Mudapedia, we take immense pride in our diverse and esteemed clientele. Our clients span various industries and sectors, each benefiting from our innovative solutions and dedicated service. We believe in building long-term relationships based on trust, transparency, and mutual growth. Here are some of the valued clients who have chosen Mudapedia as their trusted partner:
           </p>
-          <button className='px-5 py-2 bg-[#4F5CDF] text-white rounded-md' onClick={() => window.open('https://api.whatapps.com/message/62895621048269')}>
-            Contact Us!
+          <button className=' text-[#4F5CDF] rounded-md w-full justify-start flex gap-3' onClick={() => window.open('https://api.whatapps.com/message/62895621048269')}>
+            Lihat Semua Mitra Kami <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 12H20M20 12L14 6M20 12L14 18" stroke="#4F5CDF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
           </button>
         </div>
       </div>
       <div className='lg:block hidden max-w-screen-lg mx-auto'>
         <Swiper
-          slidesPerView={4}
+          slidesPerView={2}
           spaceBetween={20}
           autoplay={{ delay: 3000 }}
           modules={[Autoplay]}
         >
           {data.map((item, index) => (
-            <SwiperSlide key={index} className=' w-full flex flex-col items-start lg:h-52 bg-gray-100 rounded-xl py-4 px-5 gap-5'>
+            <SwiperSlide key={index} className=' w-full flex flex-col items-start lg:h-52 bg-gray-100 rounded-xl py-4 px-5 gap-12 cursor-pointer' onClick={()=>{
+              window.open(item.link)
+            }}>
               <img src={item.logo} alt="" width={50} height={50} />
-              <p className='text-[12px] font-medium'>{item.descriptions}</p>
+              <p className='font-semibold'>{item.name}</p>
+              <p className='text-[12px] font-normal'>{item.descriptions}</p>
             </SwiperSlide>
           ))}
         </Swiper>
@@ -61,9 +64,12 @@ const Ourclient = () => {
         <div className='grid w-full gap-5 grid-cols-2'>
           {data.map((item, index) => {
             return (
-              <div className=' w-full flex flex-col items-start lg:h-52 bg-gray-100 rounded-xl py-4 px-5 gap-5'>
+              <div className=' w-full flex flex-col items-start lg:h-52 bg-gray-100 rounded-xl py-4 px-5 gap-1 cursor-pointer' onClick={()=>{
+                window.open(item.link)
+              }}>
                 <img src={item.logo} alt="" width={50} height={50} />
-                <p className='text-[12px] font-medium'>{item.descriptions}</p>
+              <p className='font-semibold'>{item.name}</p>
+              <p className='text-[12px] font-normal'>{item.descriptions}</p>
               </div>
             )
           })}
