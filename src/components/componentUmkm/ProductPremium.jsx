7a3d40287330fdb2557b525e4690b29ef3c4b46c
IIQball <muhammadiqbl45@gmail.com>
2024-06-27 12:27:52 +0700

    new card premium component

---
+++ src/components/componentUmkm/ProductPremium.jsx
@@ -0,0 +1,94 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line react/prop-types
+import hexRgb from "hex-rgb";
+import { useEffect, useState } from "react";
+
+const ProductPremium = ({
+  product: { data, title },
+  color: { textColor, accentColor, primary },
+}) => {
+  const [hexObj, setHexObjs] = useState({});
+
+  useEffect(
+    function () {
+      let hexPrimary = primary.slice(4, -1);
+      hexPrimary = hexRgb(hexPrimary);
+
+      setHexObjs(
+        `rgba(${hexPrimary.red},${hexPrimary.green},${hexPrimary.blue},0.1)`
+      );
+    },
+    [primary]
+  );
+
+  console.log(accentColor.slice(6,13))
+  return (
+    <section
+      className={`${textColor} pt-10 pb-20 max-w-screen-lg mx-auto px-5`}
+    >
+      {title ? (
+        <p className="text-center mb-4 text-3xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-5xl">
+          {title}
+        </p>
+      ) : (
+        ""
+      )}
+      {/* card */}
+      <section className="gap-5 mt-10 lg:mt-32 w-fit grid md:grid-cols-2 lg:grid-cols-3 mx-auto">
+        {data.map((v, i) => (
+          <div
+            key={i}
+            className={`max-w-sm border border-gray-200 lg:w-80 rounded-lg shadow relative ${
+              v.price ? "pb-10" : ""
+            } break-words w-full`}
+            style={{ backgroundColor: hexObj }}
+          >
+            <div>
+              <div
+                className="w-80 h-80 rounded-lg"
+                style={{
+                  background: `url(${v.img})`,
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                }}
+              ></div>
+            </div>
+            <div className={`${v.deskripsi || v.price ? "p-5" : ""}`}>
+              {v.name ? (
+                <div>
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {v.name}
+                  </h5>
+                </div>
+              ) : (
+                ""
+              )}
+              {v.deskripsi ? (
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 max-w-[280px]">
+                  {v.deskripsi}
+                </p>
+              ) : (
+                ""
+              )}
+            </div>
+            <div className="flex justify-between absolute bottom-2 w-full px-5 items-center">
+              {v.price ? (
+                <p
+                  className={`text-xl ${accentColor} font-bold`}
+                >
+                  {v.price}
+                </p>
+              ) : (
+                ""
+              )}
+              <button className={`bg-[${accentColor.slice(6,13)}] text-white px-3 py-3 rounded-md text-xs`}>Pesan Sekarang</button>
+            </div>
+          </div>
+        ))}
+      </section>
+    </section>
+  );
+};
+
+export default ProductPremium;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    '/index.html'
+    '/index.html',
+    "./src/**/**/*.{js,jsx,ts,tsx}",
+    "./src/**/**/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
