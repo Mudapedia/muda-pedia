6ff8b872dd6c64006068698642cc707b7eea07e0
unknown <muizzuddin334@gmail.com>
2024-06-28 10:33:01 +0700

    pesan wa

--- src/components/componentUmkm/keranjangFloating.jsx
+++ src/components/componentUmkm/keranjangFloating.jsx
@@ -17,7 +17,12 @@ const KeranjangFloating = ({
   const btnPesan = (e) => {
     e.preventDefault();
 
-    alert(`nama: ${nama}\ndeskripsi: ${deskripsi}`);
+    const text = barang
+      .map((v) => `nama: ${v.name} harga: ${v.price}`)
+      .join(" ");
+
+    const waURL = `https://wa.me/6281236827185?text=${text}`;
+    window.location.href = waURL;
   };
 
   const kurangiBarang = (index, price) => {
@@ -108,7 +113,7 @@ const KeranjangFloating = ({
             })}
           </div>
 
-          <form className="px-4" onSubmit={btnPesan}>
+          <form className="px-4" onSubmit={btnPesan} method="POST">
             {/* form */}
             <div>
               <label htmlFor="nama">Nama</label>
