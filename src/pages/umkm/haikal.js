20de691f9e45caf348cee074b0b8631ac62c97f7
EkallFH <mhmmdfkrihaikall@gmail.com>
2024-07-10 22:23:49 +0700

    tambah es-teh-menantoe

---
+++ src/pages/umkm/haikal.js
@@ -0,0 +1,96 @@
+const dataHaikal = {
+  "es-teh-menantoe": {
+    title: "Es Teh Menantoe",
+    icon: null,
+    navbarTitle: "Es Teh Menantoe",
+    ctaLink: "https://wa.me/6282244972427",
+    keyword: [
+      "Es Teh Menantoe",
+      "Minuman Segar",
+      "Es Teh",
+      "Es Mojito",
+      "Boba",
+      "Es Teh Kekinian",
+      "Es Teh Segar",
+      "Minuman Boba",
+      "Mojito Segar",
+      "Minuman Hits",
+      "Es Teh Viral",
+      "Minuman Favorit",
+      "Boba Drink",
+      "Mojito Drink",
+      "Es Teh Manis",
+      "Es Teh Unik",
+      "Minuman Dingin",
+      "Es Teh Menyegarkan",
+      "Mojito Boba",
+      "Minuman Modern",
+      "Es Teh Premium",
+      "Es Teh Nusantara",
+      "Boba Lovers",
+      "Es Mojito Spesial",
+      "Minuman Populer",
+    ],
+    color: {
+      // required
+      primary: "bg-[#24915e]",
+      secondary: "bg-[#84c073]",
+      backgroundColor: "bg-[#F7FCF3]",
+      hoverColor: "hover:bg-[#368c53]",
+      textColor: "text-[#180c0c]",
+      btnTextColor: "text-white",
+      accentColor: "text-[#dcda78]",
+    },
+
+    hero: {
+      tagLine: "Segarkan Harimu dengan Menantoe!",
+      deskripsi:
+        "Kami menawarkan es teh premium yang diracik dengan teh pilihan berkualitas tinggi, mojito yang menyegarkan dengan sentuhan daun mint dan jeruk nipis, serta boba yang kenyal dan manis yang akan memanjakan lidah Anda. Setiap tegukan Menantoe menghadirkan kesegaran dan kebahagiaan, cocok untuk dinikmati di segala suasana. Mari ciptakan momen manis bersama Menantoe!",
+      btn: true, // optional
+      btnText: "Pesan sekarang",
+      img: "https://res.cloudinary.com/dtiyid0pi/image/upload/v1720599726/umkm/es-teh-menantoe/foto2_hajuov.jpg",
+    },
+    about: {
+      title: null,
+      txt: "Jangan lewatkan Kesegaran Aneka Es Kami!",
+
+      deskripsi:
+        "Es Teh Menantoe adalah usaha minuman segar yang menyajikan berbagai varian es teh dengan cita rasa khas dan kualitas terbaik. Dibuat dari bahan-bahan pilihan dan teh berkualitas tinggi, Es Teh Menantoe hadir untuk memberikan pengalaman minum teh yang menyegarkan dan memuaskan. Dengan berbagai pilihan rasa yang inovatif, kami berkomitmen untuk selalu memberikan yang terbaik bagi pelanggan kami. Es Teh Menantoe, pilihan tepat untuk setiap momen Anda!", // optional
+      img: "https://res.cloudinary.com/dtiyid0pi/image/upload/v1720599727/umkm/es-teh-menantoe/foto1_zt4uta.jpg",
+      btn: true, // optional
+      btnText: "Pesan Sekarang",
+    },
+    product: {
+      title: "Produk Terlaris Kami Nih!",
+      // required
+      data: [
+        {
+          name: "Es Teh",
+          deskripsi:
+            "Menawarkan berbagai varian rasa, menggunakan bahan alami dan berkualitas, serta menyediakan kemasan yang menarik.",
+          price: null,
+          img: "https://halalpedia.oss-ap-southeast-5.aliyuncs.com/2023/10/20231017163647-652e55af02988-es-teh-jawa.jpg",
+        },
+        {
+          name: "Mojito",
+          deskripsi: "Menggunakan bahan-bahan segar dan alami, menawarkan berbagai varian rasa, dan menghadirkan kemasan yang menarik dan ramah lingkungan",
+          price: null,
+          img: "https://img-global.cpcdn.com/recipes/48aade9148241b91/400x400cq70/photo.jpg",
+        },
+        {
+          name: "Es Boba",
+          deskripsi: "Menyediakan berbagai pilihan rasa dan topping, memastikan kualitas boba yang kenyal dan enak, serta menghadirkan kemasan yang kekinian.",
+          price: null,
+          img: "https://akcdn.detik.net.id/visual/2021/07/15/ilustrasi-boba_43.jpeg?w=720&q=90",
+        },
+      ],
+    },
+    location: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3947.389821493706!2d114.14847557501128!3d-8.36325849167385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwMjEnNDcuNyJTIDExNMKwMDknMDMuOCJF!5e0!3m2!1sid!2sid!4v1720624767686!5m2!1sid!2sid",
+    footer: {
+      noHp: null,
+      alamat: "Jl. Gajah Mada, Dusun Sawahan, Genteng Kulon, Kec. Genteng, Kabupaten Banyuwangi, Jawa Timur 68465",
+    },
+  },
+};
+
+export default dataHaikal;
