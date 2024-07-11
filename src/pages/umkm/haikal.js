09483136fed4780c115736498cd9e4a664683429
EkallFH <mhmmdfkrihaikall@gmail.com>
2024-07-11 14:06:09 +0700

    tambah seblak-tentrem

--- src/pages/umkm/haikal.js
+++ src/pages/umkm/haikal.js
@@ -73,22 +73,116 @@ const dataHaikal = {
         },
         {
           name: "Mojito",
-          deskripsi: "Menggunakan bahan-bahan segar dan alami, menawarkan berbagai varian rasa, dan menghadirkan kemasan yang menarik dan ramah lingkungan",
+          deskripsi:
+            "Menggunakan bahan-bahan segar dan alami, menawarkan berbagai varian rasa, dan menghadirkan kemasan yang menarik dan ramah lingkungan",
           price: null,
           img: "https://img-global.cpcdn.com/recipes/48aade9148241b91/400x400cq70/photo.jpg",
         },
         {
           name: "Es Boba",
-          deskripsi: "Menyediakan berbagai pilihan rasa dan topping, memastikan kualitas boba yang kenyal dan enak, serta menghadirkan kemasan yang kekinian.",
+          deskripsi:
+            "Menyediakan berbagai pilihan rasa dan topping, memastikan kualitas boba yang kenyal dan enak, serta menghadirkan kemasan yang kekinian.",
           price: null,
           img: "https://akcdn.detik.net.id/visual/2021/07/15/ilustrasi-boba_43.jpeg?w=720&q=90",
         },
       ],
     },
-    location: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3947.389821493706!2d114.14847557501128!3d-8.36325849167385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwMjEnNDcuNyJTIDExNMKwMDknMDMuOCJF!5e0!3m2!1sid!2sid!4v1720624767686!5m2!1sid!2sid",
+    location:
+      "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3947.389821493706!2d114.14847557501128!3d-8.36325849167385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwMjEnNDcuNyJTIDExNMKwMDknMDMuOCJF!5e0!3m2!1sid!2sid!4v1720624767686!5m2!1sid!2sid",
     footer: {
       noHp: null,
-      alamat: "Jl. Gajah Mada, Dusun Sawahan, Genteng Kulon, Kec. Genteng, Kabupaten Banyuwangi, Jawa Timur 68465",
+      alamat:
+        "Jl. Gajah Mada, Dusun Sawahan, Genteng Kulon, Kec. Genteng, Kabupaten Banyuwangi, Jawa Timur 68465",
+    },
+  },
+  "seblak-tentrem": {
+    title: "Seblak Tentrem",
+    icon: null,
+    navbarTitle: "Seblak Tentrem",
+    ctaLink: null,
+    keyword: [
+      "Seblak Tentrem",
+      "Kuliner Pedas",
+      "Seblak",
+      "Mie Setan",
+      "Inoki",
+      "Seblak Kekinian",
+      "Seblak Pedas",
+      "Mie Pedas",      
+      "Seblak Viral",
+      "Kuliner Favorit",
+      "Mie Setan Pedas",
+      "Inoki Khas",
+      "Seblak Mie",
+      "Seblak Premium",
+      "Seblak Lezat",
+      "Kuliner Spesial",
+      "Mie Setan Nikmat",
+      "Inoki Unik",
+      "Seblak Populer",
+    ],
+    color: {
+      // required
+      primary: "bg-[#be3530]",
+      secondary: "bg-[#ea5856]",
+      backgroundColor: "bg-[#FCF6F7]",
+      hoverColor: "hover:bg-[#642331]",
+      textColor: "text-[#180c0c]",
+      btnTextColor: "text-white",
+      accentColor: "text-[#d3c734]",
+    },
+
+    hero: {
+      tagLine: "Rasakan Kedamaian dalam Setiap Gigitan!",
+      deskripsi:
+        "Selamat datang di Seblak Tentrem, tempat di mana cita rasa autentik dan kelezatan berpadu untuk menciptakan momen yang tentram. Nikmati hidangan khas kami, Seblak Tentrem, yang penuh dengan rasa pedas gurih dan kenikmatan sejati. Ayo, segarkan hari Anda dan temukan kedamaian dalam setiap gigitan di Seblak Tentrem!",
+      btn: false, // optional
+      btnText: "Pesan sekarang",
+      img: "https://res.cloudinary.com/dtiyid0pi/image/upload/v1719888408/umkm/seblak%20tentrem/WhatsApp_Image_2024-07-01_at_13.22.03_7bc9c2e0_f8sflb.jpg",
+    },
+    about: {
+      title: null,
+      txt: "Kenikmatan Pedas yang Menggoda",
+
+      deskripsi:
+        "Seblak Tentrem adalah usaha kuliner yang menawarkan sensasi pedas dan lezat dalam setiap gigitan. Kami menyajikan berbagai menu andalan seperti seblak tentrem, mie setan, dan inoki yang diracik dengan bumbu khas dan bahan-bahan berkualitas. Dibuat dengan penuh cinta dan perhatian, setiap hidangan di Seblak Tentrem dijamin memberikan pengalaman kuliner yang memuaskan dan tak terlupakan.", // optional
+      img: "https://res.cloudinary.com/dtiyid0pi/image/upload/v1719888408/umkm/seblak%20tentrem/WhatsApp_Image_2024-07-01_at_13.22.03_7bc9c2e0_f8sflb.jpg",
+      btn: false, // optional
+      btnText: "Pesan Sekarang",
+    },
+    product: {
+      title: "Produk Kami",
+      // required
+      data: [
+        {
+          name: "Seblak Tentrem",
+          deskripsi:
+            "Seblak tentrem adalah seblak khas yang dibuat dari kerupuk basah yang dimasak dengan bumbu pedas, telur, sayuran, dan tambahan protein seperti sosis, bakso, atau ayam.",
+          price: null,
+          img: "https://guide.horego.com/wp-content/uploads/2024/02/seblak-teh-erna-featured-jpeg.webp",
+        },
+        {
+          name: "Mie Setan",
+          deskripsi:
+            "Mie setan adalah hidangan mie dengan bumbu super pedas yang biasanya dilengkapi dengan berbagai topping seperti bakso, pangsit, dan sayuran.",
+          price: null,
+          img: "https://assets.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2023/02/20/mie-setan-2372051745.jpg",
+        },
+        {
+          name: "Enoki",
+          deskripsi:
+            "Menyediakan berbagai pilihan rasa dan topping, memastikan kualitas boba yang kenyal dan enak, serta menghadirkan kemasan yang kekinian.",
+          price: null,
+          img: "https://asset.kompas.com/crops/El_1Y87Vr4H9ZtE8l3DDwkUuRIo=/52x70:948x667/750x500/data/photo/2021/07/02/60deac4db581e.jpg",
+        },
+      ],
+    },
+    location:
+    "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3948.801138673385!2d114.3564308733235!3d-8.222743515155676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwMTMnMjEuOSJTIDExNMKwMjEnMjMuMyJF!5e0!3m2!1sid!2sid!4v1720681439245!5m2!1sid!2sid",
+    footer: {
+      noHp: null,
+      alamat:
+        "Jl. K.H. Agus Salim 114-112, Lingkungan Cuking Rw., Mojopanggung, Kec. Giri, Kabupaten Banyuwangi, Jawa Timur 68425",
     },
   },
 };
