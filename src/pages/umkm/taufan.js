879b97b4ae276a33b576aa02a97f3e3ea9e6edc7
alvariezi <148828942+alvariezi@users.noreply.github.com>
2024-07-11 12:04:26 +0700

    tambah kedai-elnau

--- src/pages/umkm/taufan.js
+++ src/pages/umkm/taufan.js
@@ -74,6 +74,94 @@ const dataTaufan = {
         alamat: "Jl. Kertanegara 7, Kebalenan, Kec. Banyuwangi, Kabupaten Banyuwangi, Jawa Timur 68417",
       },
     },
+    "kedai-elnau" : {   
+      title: "Kedai elnau",
+      icon: null,
+      navbarTitle: "Kedai Elnau",
+      ctaLink: null,
+      keyword: [
+       "Tahu Walik Enak",
+       "Tahu Walik Renyah",
+       "Tahu Walik Spesial",
+       "Tahu Walik Kedai Elnau",
+       "Es Mojito Segar",
+       "Es Mojito Kedai Elnau",
+       "Es Boba Manis",
+       "Es Boba Segar",
+       "Es Boba Kedai Elnau",
+       "Tahu Walik dan Es Mojito",
+       "Tahu Walik dan Es Boba",
+       "Menu Favorit Kedai Elnau",
+       "Kedai Elnau Tahu Walik",
+       "Kedai Elnau Es Mojito",
+       "Kedai Elnau Es Boba",
+       "Makanan dan Minuman Segar",
+       "Kudapan dan Minuman Hits",
+       "Tempat Nongkrong Asyik",  
+       "Cemilan Nikmat Kedai Elnau",
+       "Minuman Segar dan Cemilan Renyah"
+      ]
+      ,
+      color: {
+        // required
+        primary: "bg-[#cd6d37]",
+        secondary: "bg-[#cd6d37]",
+        backgroundColor: "bg-[#faf1f1]",
+        hoverColor: "hover:bg-[#012306]",
+        textColor: "text-[#180c0c]",
+        btnTextColor: "text-white",
+        accentColor: "text-[#e85a5a]",
+      },
+  
+      hero: {
+        tagLine: "Nikmati Hidangan Terbaik di Kedai Elnau",
+        deskripsi: "Temukan kenikmatan dalam setiap sajian kami. Dari Tahu Walik yang renyah hingga Es Mojito dan Es Boba yang segar",
+        btn: null, // optional
+        btnText: "Pesan sekarang",
+        img:"https://res.cloudinary.com/dtiyid0pi/image/upload/v1719886551/umkm/kedai%20elnau/WhatsApp_Image_2024-07-01_at_13.20.26_b5273b64_rzdtip.jpg" ,
+      },
+      about: {
+        title: null,
+        txt: "Nikmati Kelezatan di Kedai Elnau",
+  
+        deskripsi: " Nikmati suasana hangat dan nyaman sambil menikmati hidangan favorit Anda di sini.  ", // optional
+        img: "https://res.cloudinary.com/dtiyid0pi/image/upload/v1719886549/umkm/kedai%20elnau/WhatsApp_Image_2024-07-01_at_13.20.25_d32f9205_stwe98.jpg",
+        btn: false, // optional
+        btnText: "Pesan Sekarang",
+      },
+      product: {
+        title: "Produk Terlaris Kami Nih!",
+        // required
+        data: [
+          {
+            name: "Es Mojito",
+            deskripsi:
+              "Es Mojito adalah minuman menyegarkan yang terbuat dari campuran air soda, jeruk nipis, daun mint segar, dan gula.",
+            price: null,
+            img: "https://img-global.cpcdn.com/recipes/48aade9148241b91/400x400cq70/photo.jpg",
+          },
+          {
+            name: "Es Boba",
+            deskripsi:
+              "Es Boba adalah minuman yang menyegarkan dan populer, terbuat dari teh atau susu yang dicampur dengan bola-bola tapioka kenyal (boba)",
+            price: null,
+            img: "https://d1bpj0tv6vfxyp.cloudfront.net/articles/92ff41ac-ea3d-427e-93d7-9e48736f8482_article_image_url.webp",
+          },
+          {
+            name: "Tahu Walik",
+            deskripsi:
+              "Tahu Walik adalah kudapan unik dari tahu yang dibalik, sehingga bagian dalamnya menjadi luar dan digoreng hingga renyah.",
+            price: null,
+            img: "https://jadesta.kemenparekraf.go.id/imgpost/80271.jpg",
+          },
+        ],
+      },
+      location:"https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3948.695720178091!2d114.35080217500973!3d-8.233321591799628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwMTMnNjAuMCJTIDExNMKwMjEnMTIuMiJF!5e0!3m2!1sid!2sid!4v1720620014169!5m2!1sid!2sid" ,
+      footer: {
+        noHp: null,
+        alamat: "Banyuwangi, Kebalenan, Kec. Banyuwangi, Kabupaten Banyuwangi, Jawa Timur 68417",
+      },
+    }
   };
   
   export default dataTaufan;
