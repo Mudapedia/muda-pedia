fe61e4bb6f55785da3f4dea1b2ca5758dc733b17
TriKeren <160460241+TriKeren@users.noreply.github.com>
2024-07-11 14:06:18 +0700

    jenang-bu-sri

--- src/pages/umkm/tri.js
+++ src/pages/umkm/tri.js
@@ -68,6 +68,89 @@ const dataTri = {
       alamat: "Jl. Kertanegara 7, Kebalenan, Kec. Banyuwangi",
     },
   },
+  "jenang-bu-sri": {
+    title: "Jenang Bu Sri",
+    icon: null,
+    navbarTitle: "Jenang Bu Sri",
+    ctaLink: null,
+    keyword: [
+      "Jenang Bu Sri",
+      "Jenang Tradisional",
+      "Jenang Khas Jawa",
+      "Jenang Enak",
+      "Jenang Manis",
+      "Jenang Gurih",
+      "Jenang Asli",
+      "Jenang Ketan",
+      "Jenang Pandan",
+      "Jenang Kacang Hijau",
+      "Jenang Gula Merah",
+      "Jenang Kelapa",
+      "Jenang Pisang",
+      "Jenang Segar",
+      "Jenang Lembut"
+    ]
+    ,
+    color: {
+      // required
+      primary: "bg-[#FF3333]",
+      secondary: "bg-[#DD8B8B]",
+      backgroundColor: "bg-[#FDF9F9]",
+      hoverColor: "hover:bg-[#DD8B8B]",
+      textColor: "text-[#1E0B0B]",
+      btnTextColor: "text-white",
+      accentColor: "text-[#D66767]",
+    },
+
+    hero: {
+      tagLine: "Cita Rasa Manis dari Tradisi Kuliner Jawa",
+      deskripsi: "Jenang Bu Sri adalah salah satu kuliner tradisional khas Jawa yang terkenal dengan cita rasa manis dan tekstur lembutnya. Jenang, sejenis dodol atau bubur manis yang terbuat dari campuran beras ketan, santan, gula kelapa, dan bahan-bahan alami lainnya, disajikan dalam berbagai variasi rasa dan warna",
+      btn: false, // optional
+      btnText: "Pesan sekarang",
+      img: "https://res.cloudinary.com/dtiyid0pi/image/upload/v1719890051/umkm/jenang%20bu%20sri/WhatsApp_Image_2024-07-01_at_13.32.23_d1e781e5_oqbgj1.jpg",
+    },
+    about: {
+      title: "Tentang Kami",
+      txt: "Lezatnya Jenang Bu Sri, Cita Rasa Tradisional yang Menggugah Selera!",
+
+      deskripsi: "Rasakan kelezatan warisan kuliner Nusantara dengan Jenang Bu Sri! Dibuat dari bahan-bahan pilihan dan resep turun-temurun, Jenang Bu Sri menawarkan cita rasa manis yang sempurna dan tekstur lembut yang memanjakan lidah", // optional
+      img: "https://res.cloudinary.com/dtiyid0pi/image/upload/v1719890051/umkm/jenang%20bu%20sri/WhatsApp_Image_2024-07-01_at_13.32.23_d1e781e5_oqbgj1.jpg",
+      btn: true, // optional
+      btnText: "Pesan Sekarang",
+    },
+    product: {
+      title: "Produk Kami!",
+      // required
+      data: [
+        {
+          name: "Jenang",
+          deskripsi:
+            "Jenang adalah salah satu hasil olahan dari tepung ketan. Selain tepung ketan, dalam pembuatan jenang diperlukan bahan tambahan berupa gula merah dan santan kelapa.",
+          price: null,
+          img: "https://assets.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2023/04/02/1-2909986846.jpeg",
+        },
+        {
+          name: "Sumsum",
+          deskripsi:
+            "Makanan tradisional Indonesia yang populer. Bubur ini terbuat dari bahan dasar tepung ketan atau tepung beras.",
+          price: null,
+          img: "https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2023/07/11045117/X-Resep-Bubur-Sumsum-yang-Lembut-dan-Mudah-Dibuat.jpg.webp",
+        },
+        {
+          name: "Kacang Ijo",
+          deskripsi:
+            "Sajian lezat yang sehat dan kaya manfaat",
+          price: null,
+          img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQX5R6Q1lEfuEmRrJvq7N2CqItLqC8JD-8TSg&s",
+        },
+      ],
+    },
+    location: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3948.8367539597593!2d114.3514528746483!3d-8.219166691813303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwMTMnMDkuMCJTIDExNMKwMjEnMTQuNSJF!5e0!3m2!1sid!2sid!4v1720681183972!5m2!1sid!2sid",
+    footer: {
+      noHp: null,
+      alamat: "Jl. K.H. Agus Salim No.138, Lingkungan Cuking Rw., Mojopanggung, Kec. Giri,",
+    },
+  },
 };
 
 export default dataTri;
